118d9b3d73d059db15cf426f4d97e3a4decf86cb
Sami Keijonen <sami.keijonen@foxnet.fi>
2012-12-28 11:58:48 +0200

    Download features and doc taxonomy were mixed up.

--- includes/taxonomy.php
+++ includes/taxonomy.php
@@ -1,40 +1,40 @@
 <?php
 /**
- * Setup Doc Taxonomies
+ * Setup Download Taxonomies
  *
- * Registers the custom taxonomy 'doc' post type.
+ * Registers the custom taxonomy download features.
  *
  * @since 0.1.0
 
 */
 
-add_action( 'init', 'foxnet_themes_shop_plugin_taxonomies' );
+add_action( 'init', 'edd_download_info_taxonomies' );
 
-function foxnet_themes_shop_plugin_taxonomies() {
-	$doc_category_labels = array(
-		'name' 				=> _x( 'Doc Categories', 'taxonomy general name', 'foxnet-themes-shop-plugin' ),
-		'singular_name' 	=> _x( 'Doc category', 'taxonomy singular name', 'foxnet-themes-shop-plugin' ),
-		'search_items' 		=> __( 'Search Doc Categories', 'foxnet-themes-shop-plugin' ),
-		'all_items' 		=> __( 'All Doc Categories', 'foxnet-themes-shop-plugin' ),
-		'parent_item' 		=> __( 'Parent Doc Category', 'foxnet-themes-shop-plugin' ),
-		'parent_item_colon' => __( 'Parent Doc Category:', 'foxnet-themes-shop-plugin' ),
-		'edit_item' 		=> __( 'Edit Doc Categories', 'foxnet-themes-shop-plugin' ), 
-		'update_item' 		=> __( 'Update Doc Category', 'foxnet-themes-shop-plugin' ),
-		'add_new_item' 		=> __( 'Add New Doc Category', 'foxnet-themes-shop-plugin' ),
-		'new_item_name' 	=> __( 'New Doc Category Name', 'foxnet-themes-shop-plugin' ),
-		'menu_name' 		=> __( 'Doc Categories', 'foxnet-themes-shop-plugin' ),
+function edd_download_info_taxonomies() {
+	$feature_labels = array(
+		'name' 				=> _x( 'Download Features', 'taxonomy general name', 'edd-download-info' ),
+		'singular_name' 	=> _x( 'Feature', 'taxonomy singular name', 'edd-download-info' ),
+		'search_items' 		=> __( 'Search Features', 'edd-download-info' ),
+		'all_items' 		=> __( 'All Features', 'edd-download-info' ),
+		'parent_item' 		=> __( 'Parent Feature', 'edd-download-info' ),
+		'parent_item_colon' => __( 'Parent Feature:', 'edd-download-info' ),
+		'edit_item' 		=> __( 'Edit Features', 'edd-download-info' ), 
+		'update_item' 		=> __( 'Update Feature', 'edd-download-info' ),
+		'add_new_item' 		=> __( 'Add New Feature', 'edd-download-info' ),
+		'new_item_name' 	=> __( 'New Feature Name', 'edd-download-info' ),
+		'menu_name' 		=> __( 'Features', 'edd-download-info' ),
 	); 	
 
-	$doc_category_args = apply_filters( 'edd_download_info_feature_args', array(
+	$feature_args = apply_filters( 'edd_download_info_feature_args', array(
 			'hierarchical' 	=> true,
-			'labels' 		=> apply_filters( 'foxnet_themes_shop_plugin_doc_labels', $doc_category_labels ),
+			'labels' 		=> apply_filters( 'edd_download_info_feature_labels', $feature_labels ),
 			'show_ui' 		=> true,
-			'query_var' 	=> 'doc_category',
-			'rewrite' 		=> array( 'slug' => 'docs/for' )
+			'query_var' 	=> 'download_feature',
+			'rewrite' 		=> array( 'slug' => 'downloads/feature' )
 		)
 	);
 
-	register_taxonomy( 'doc_category', array( 'doc' ), $doc_category_args );
+	register_taxonomy( 'edd_download_info_feature', array( 'download' ), $feature_args );
 	
 }
 
